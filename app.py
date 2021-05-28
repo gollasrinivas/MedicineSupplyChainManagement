e17eed2101930c6954d989ff879e29e9586f2dc3
srinivas <gsrinivas0000@gmail.com>
2021-05-28 09:27:02 +0530

    commit

--- app.py
+++ app.py
@@ -1,28 +1,60 @@
 from flask import Flask,render_template,request,redirect,url_for
+import requests
+import json
 app=Flask(__name__)
-d={
-    "MD01":{"id":"MD01","name":"dolo","price":350,"status":"production stage"},
-   "MD02":{"id":"MD02","name":"pan","price":400,"status":"transport stage"}
-   }
+
 @app.route('/',methods=["GET","POST"])
 def hello():
     if request.method=="GET":
         return render_template('home.html')
     else:
         val=request.form['med']
-        if val in d:
-            m=d[val]
-            return render_template('home1.html',flag=1,id=m['id'],name=m['name'],price=m["price"],status=m["status"])
+        r=requests.get('http://localhost:8080/api/query/'+str(val))
+        print(r.json())
+        if r.status_code == 200:
+            m=r.json()['response']
+            print(m)
+            return render_template('home1.html',flag=1,med=m)
         else:
+            print("fast")
             return render_template('home1.html',flag=0)
 
+@app.route('/admin',methods=["GET","POST"])
+def admin():
+    if request.method=="GET":
+        
+        return render_template('admin.html',flag=0)
+    else:
+        obj={
+            "carid":request.form["carid"],
+            "make":request.form["make"],
+            "model":request.form["model"],
+            "colour":request.form["colour"],
+            "owner":request.form["owner"]
+        }
+              
+
+        requests.post('http://localhost:8080/api/addcar/',data=obj)
+        return render_template("admin.html",flag=1)
+
+@app.route('/change',methods=["POST"])
+def change():
+    res=requests.put('http://localhost:8080/api/changeowner/'+str(request.form["pid"]),data={"owner":request.form["powner"]})
+    return render_template("admin.html",flag=2)
 
 
-@app.route('/track-medicine',methods=["GET","POST"])
-def track_medicine():
-    d={"MD01":{"id":"MD01","name":"dolo","price":350}}
-    if request.method=="POST":
-        return redirect(url_for('.hello',a=d))
+@app.route('/display',methods=["GET"])
+def display():
+    res=requests.get('http://localhost:8080/api/queryallcars')
+    print("-------------------")
+    print(res.json())
+    print("aaa")
+    return render_template("admin.html",flag=3,allmed=res.json()['response'])
+
+        
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ input.py
@@ -0,0 +1,12 @@
+d={
+    "MD01":{"id":"MD01","name":"dolo","price":350,"status":"production stage"},
+   "MD02":{"id":"MD02","name":"pan","price":400,"status":"transport stage"},
+   "MD03":{"id":"MD03","name":"vicks","price":50,"status":"retailer stage"},
+   "MD04":{"id":"MD04","name":"paracetamol","price":40,"status":"transport stage"},
+   "MD05":{"id":"MD05","name":"Abc","price":35,"status":"production stage"},
+   "MD06":{"id":"MD06","name":"covaxin","price":"free","status":"Trail stage"},
+   "MD07":{"id":"MD07","name":"antibiotical","price":400,"status":"storage stage"},
+   "MD08":{"id":"MD09","name":"vitaminC","price":40,"status":"storage stage"},
+   "MD09":{"id":"MD08","name":"antiiral","price":530,"status":"storage stage"}
+   }
+pointer=9
